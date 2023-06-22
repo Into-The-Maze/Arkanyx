8badafda45c88a41a5246808f208eef079ba12e8
Jonty <89488176+Jomlom@users.noreply.github.com>
2023-06-22 13:23:16 +0100

    actual movement and jumping
    with gravity!!!

--- Arkaynx/Assets/Scripts/CharacterMovement.cs
+++ Arkaynx/Assets/Scripts/CharacterMovement.cs
@@ -4,28 +4,55 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-    private CharacterController characterController;
+    private CharacterController body;
     private Transform player;
     private Camera cam;
 
-    public float speed = 5f;
+    public float speed = 5;
+    private bool isGrounded;
+    private Vector3 playerVerticalVelocity = Vector3.zero;
+    private float gravityValue = -20f;  
+    private float jumpHeight = 1.0f;
+    private CapsuleCollider floorDetector;
 
     void Start()
     {
-        characterController = GetComponent<CharacterController>();
+        body = GetComponent<CharacterController>();
         player = GetComponent<Transform>();
         cam = Camera.main;
+        floorDetector = GetComponent<CapsuleCollider>();
     }
     void Update() {
         UpdateMoveHorizontal();
+        UpdateMoveVertical();
     }
-    
+
+    private void OnTriggerStay(Collider other) {
+        if (other.tag == "Ground") { isGrounded = true; }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.tag == "Ground") { isGrounded = false; }
+    }
+
     private void UpdateMoveHorizontal() {
         var forward = cam.transform.forward;
         var right = cam.transform.right;
         forward.y = 0;
         right.y = 0;
 
-        characterController.Move(speed * Time.deltaTime * ((Input.GetAxis("Horizontal") * right) + (Input.GetAxis("Vertical") * forward)));
+        body.Move(speed * Time.deltaTime * ((Input.GetAxis("Horizontal") * right) + (Input.GetAxis("Vertical") * forward)).normalized);
+    }
+
+    private void UpdateMoveVertical() {
+        Debug.Log(isGrounded);
+        if (isGrounded && playerVerticalVelocity.y < 0) {
+            playerVerticalVelocity.y = 0;
+        }
+        if (Input.GetButtonDown("Jump") && isGrounded) {
+            playerVerticalVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+        playerVerticalVelocity.y += gravityValue * Time.deltaTime;
+        body.Move(playerVerticalVelocity * Time.deltaTime);
     }
 }
